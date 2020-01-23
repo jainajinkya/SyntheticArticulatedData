Major Bug Fix: Updated change_frames function to reflect the quaternion convetion used in MuJoCO (wxyz, instead of xyzw)

generation/generator_v2.py
@@ -261,11 +261,11 @@ class SceneGenerator():
                 torque_vals.append(copy.copy(sim.data.qfrc_applied[:n_qpos_variables]))
                 applied_forces.append(copy.copy(force))
                 x_pos = np.append(sim.data.get_body_xpos("handle_link"), sim.data.get_body_xquat("handle_link"))
-                moving_frame_xpos_world.append(copy.copy(x_pos))
-                joint_frame_in_world = np.append(sim.data.get_body_xpos("cabinet_left_hinge"),
-                                                 obj.rotation)
-                moving_frame_xpos_ref_frame.append(
-                    change_frames(frame_B_wrt_A=joint_frame_in_world, pose_wrt_A=x_pos))
+                moving_frame_xpos_world.append(copy.copy(x_pos))  # quat comes in wxyz form
+                joint_frame_in_world = np.append(copy.copy(sim.data.get_body_xpos("cabinet_left_hinge"),
+                                                 obj.rotation))
+                moving_frame_xpos_ref_frame.append(copy.copy(
+                    change_frames(frame_B_wrt_A=joint_frame_in_world, pose_wrt_A=x_pos)))
 
                 img, depth = sim.render(IMG_WIDTH, IMG_HEIGHT, camera_name='external_camera_0', depth=True)
                 depth = vertical_flip(depth)

generation/mujocoMicrowaveParts.py
@@ -235,7 +235,7 @@ def test():
     t = 0
     # sim.data.ctrl[0] = - 0.2
     sim.data.ctrl[0] = 0.
-    force = np.array([-1., 0.0, 0.])
+    force = np.array([-2., 0.0, 0.])
     torque = np.array([0., 0., 0.0])
     pt = sim.data.get_body_xpos("handle_link")
     bodyid = sim.model.body_name2id("handle_link")
@@ -248,8 +248,11 @@ def test():
         # print("Cartesian position: ", sim.data.body_xpos)
         sim.step()
         viewer.render()
-        print("Applied force: ", force)
+        # print("Applied force: ", force)
         # print("Applied force from scene: ", sim.data.xfrc_applied[bodyid, :])
+
+        if t % 250:
+            print(sim.data.get_body_xpos("handle_link"), sim.data.get_body_xquat("handle_link"))
         t += 1
 
 

generation/utils.py
@@ -164,11 +164,9 @@ def sample_pose_drawer():
 
 def change_frames(frame_B_wrt_A, pose_wrt_A):
     A_T_pose = tf3d.affines.compose(T=pose_wrt_A[:3],
-                                    R=tf3d.quaternions.quat2mat(
-                                        [pose_wrt_A[6], pose_wrt_A[3], pose_wrt_A[4], pose_wrt_A[5]]),
-                                    # tf3d takes quat in  wxyz
+                                    R=tf3d.quaternions.quat2mat(pose_wrt_A[3:]),  # quat in  wxyz
                                     Z=np.ones(3))
-    A_rot_mat_B = tf3d.quaternions.quat2mat([frame_B_wrt_A[6], frame_B_wrt_A[3], frame_B_wrt_A[4], frame_B_wrt_A[5]])
+    A_rot_mat_B = tf3d.quaternions.quat2mat(frame_B_wrt_A[3:])
 
     # Following as described in Craig
     B_T_A = tf3d.affines.compose(T=-A_rot_mat_B.T.dot(frame_B_wrt_A[:3]),
@@ -178,4 +176,4 @@ def change_frames(frame_B_wrt_A, pose_wrt_A):
     B_T_pose = B_T_A.dot(A_T_pose)
     trans, rot, scale, _ = tf3d.affines.decompose44(B_T_pose)
     quat = tf3d.quaternions.mat2quat(rot)
-    return np.concatenate((trans, np.array([quat[1], quat[2], quat[3], quat[0]])))  # return in xyzw
+    return np.concatenate((trans, quat))  # return quat in wxyz