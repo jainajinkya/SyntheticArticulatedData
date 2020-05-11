8b679481e4e8d89d00f11ea4068335b2677221fa
Ajinkya Jain <jainajinkya92@gmail.com>
2020-05-11 17:00:41 +0530

    Added function to visualize dataset

--- generation/dataset_utils.py
+++ generation/dataset_utils.py
@@ -1,7 +1,15 @@
+import os
+import cv2
 import numpy as np
 import h5py
 import copy
 import argparse
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+from mujoco_py.modder import TextureModder
+
+import SyntheticArticulatedData.generation.calibrations as calibrations
+from SyntheticArticulatedData.generation.generator_v2 import vertical_flip, buffer_to_real, white_bg
+from SyntheticArticulatedData.generation.mujocoDoubleCabinetParts import set_two_door_control
 
 
 def combine_datasets(filenames, output_dir):
@@ -35,20 +43,79 @@ def subsample_dataset(file_in, sub_size, output_dir):
         print("Created subsampled dataset file at: {}".format(output_dir + '/complete_data.hdf5'))
 
 
+def debug_sample(filename, obj_type, savedir, img_idx=0, masked=False):
+    # Load image in mujoco
+    model = load_model_from_path(filename)
+    sim = MjSim(model)
+    modder = TextureModder(sim)
+
+    if obj_type == 4 or obj_type == 5:
+        # MULTI CABINET: get double the params.
+        set_two_door_control(sim, 'cabinet2' if obj_type == 4 else 'refrigerator')
+    else:
+        if obj_type == 1:
+            sim.data.ctrl[0] = 0.05
+        # elif obj.geom[3] == 1:
+        #     sim.data.ctrl[0] = -0.2
+        else:
+            sim.data.ctrl[0] = 0.2
+
+    t = 0
+    #########################
+    IMG_WIDTH = calibrations.sim_width
+    IMG_HEIGHT = calibrations.sim_height
+    #########################
+
+    while t < 4000:
+        sim.forward()
+        sim.step()
+
+        if t % 500 == 0:
+            img, depth = sim.render(IMG_WIDTH, IMG_HEIGHT, camera_name='external_camera_0', depth=True)
+            img = vertical_flip(img)
+            img = white_bg(img)
+            imgfname = os.path.join(savedir, 'img' + str(img_idx).zfill(6) + '.png')
+            cv2.imwrite(imgfname, img)
+            img_idx += 1
+
+        t += 1
+    return img_idx
+
+
+def debug_samples_using_ids(data_dir, obj_type, sample_ids, masked=False):
+    img_idx = sample_ids[0] * 8  # Current frequency set at 8 images per object
+    for i in sample_ids:
+        fname = os.path.join(data_dir, 'scene' + str(i).zfill(6) + '.xml')
+        img_idx = debug_sample(fname, obj_type, data_dir, img_idx, masked=masked)
+    print("Images generated and saved in {}".format(data_dir))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Datasets utils. Available functions: combine_datasets, "
                                                  "split_datasets")
     parser.add_argument('-i', '--input_files', type=str, nargs='+',
-                        help='Input Dataset name(s), separated by spaces')
+                        help='Input Dataset filename(s) or directories(s), separated by spaces')
     parser.add_argument('-o', '--output-dir', type=str, help='path to output dir')
+
     parser.add_argument('-c', '--combine', action='store_true', default=False, help='Combine provided datasets')
+
     parser.add_argument('-sub', '--subsample', action='store_true', default=False, help='Randomly subsample dataset')
     parser.add_argument('-ns', '--sub_size', type=int, default=0, help='New size of subsampled dataset')
+
+    parser.add_argument('-d', '--debug-dataset', action='store_true', default=False,
+                        help='Should generate images to visualize samples or not?. Input file argument corresponds '
+                             'to input directory where scene files are stored.')
+    parser.add_argument('--obj-id', type=int, default=0, help='Object class index')
+    parser.add_argument('-sid', '--sample-ids', type=int, nargs='+', help='Sample ids to visualize')
+    parser.add_argument('--masked', action='store_true', default=False, help='Masked image?')
 
     args = parser.parse_args()
     if args.combine:
         combine_datasets(filenames=args.input_files, output_dir=args.output_dir)
     elif args.subsample:
         subsample_dataset(file_in=args.input_files, sub_size=args.sub_size, output_dir=args.output_dir)
+    elif args.debug_dataset:
+        debug_samples_using_ids(data_dir=args.input_files[0], obj_type=args.obj_id, sample_ids=args.sample_ids,
+                                masked=args.masked)
     else:
-        print("Only combine and subsample functions implemented yet!")
+        print("Function implemented yet: combine, subsample, debug_sample!")
