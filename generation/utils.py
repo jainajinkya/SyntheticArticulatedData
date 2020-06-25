b62d0322636e95b9720c29eea8528fce3c54708e
Ajinkya Jain <jainajinkya92@gmail.com>
2020-06-25 15:23:14 -0500

    Correcting pose sampling params and control value

--- generation/utils.py
+++ generation/utils.py
@@ -152,7 +152,7 @@ def sample_pose_sapien():
     xyz = pyro.sample('origin', dist.Uniform(torch.tensor([1.75, -0.5, -0.7]), torch.tensor([2.75, 0.5, 0.3]))).numpy()
     angle_x = pyro.sample('angle_x', dist.Uniform(-np.pi/4, np.pi/4)).item()
     # angle_y = pyro.sample('angle_y', dist.Beta(0.5, 0.5)).item() * np.pi/2  # Setting range to [0, pi/2]
-    angle_y = pyro.sample('angle_y', dist.Uniform(0., np.pi/2)).item()  # Setting range to [0, pi/2]
+    angle_y = pyro.sample('angle_y', dist.Uniform(1/4 * np.pi, 3/4 * np.pi)).item()  # Setting range to [0, pi/2]
     angle_z = pyro.sample('angle_z', dist.Uniform(-1 / 4 * 3.14, 1 / 4 * 3.14)).item()
     return tuple(xyz), angle_x, angle_y, angle_z
 
